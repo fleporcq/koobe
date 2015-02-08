61e8811b2e5a210817b2b7546bbf173a37bb00d2
François LEPORCQ <fleporcq@gmail.com>
2015-02-08 23:28:18 +0100

    Feat(Auth): Partage de l'utilisateur connecté entre les vues et les controleurs

--- app/Http/Controllers/KoobeController.php
+++ app/Http/Controllers/KoobeController.php
@@ -1,16 +1,22 @@
 <?php namespace App\Http\Controllers;
 
 use Config;
+use Illuminate\Auth\Guard;
 use Route;
 use View;
 
 class KoobeController extends Controller
 {
-    public function __construct()
+
+    protected $connectedUser;
+
+    public function __construct(Guard $auth)
     {
+        $this->connectedUser = $auth->user();
         $this->middleware('auth');
         View::share('appName', Config::get('app.name'));
         View::share('currentAction', Route::current()->getActionName());
+        View::share('connectedUser', $this->connectedUser);
     }
 
     protected function notFoundIfNull($object)
