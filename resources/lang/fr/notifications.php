3a4e73e838fcfd4e36bd940d41714c57c3b0d8a0
François LEPORCQ <fleporcq@gmail.com>
2015-02-19 09:58:01 +0100

    fix: casse

--- resources/lang/fr/notifications.php
+++ resources/lang/fr/notifications.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'epubFileNotFound' => 'Le fichier ":file" n\'a pas été trouvé. ',
-    'notValidEpub' => 'le fichier ":file" n\'est pas un epub valide. ',
+    'notValidEpub' => 'Le fichier ":file" n\'est pas un epub valide. ',
     'containerFileNotFound' => 'Le container du fichier ":file" n\'a pas été trouvé. ',
     'rootFileNotFound' => 'Le root du fichier ":file" n\'a pas été trouvé. ',
     'bookAlreadyStored' => 'Le livre ":file" a déjà été ajouté à la bibliothèque. ',
